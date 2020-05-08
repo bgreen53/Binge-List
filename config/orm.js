a2a617e3fd1afa3cd5ed9bc24b587e88fecdf1d7
Barry Green <Barry.Green@SLFHC.ORG>
2020-05-08 16:59:53 -0700

    added to orm

---
+++ config/orm.js
@@ -0,0 +1,12 @@
+var connection = require("./connection.js");
+
+var orm = {
+    selectAll: function(table, cb){
+        var qs = SELECT * from ??
+        connection.query(qs,[table], function(err,res){
+            if (err) throw err;
+            
+            cb(res)
+        })
+    }
+}
